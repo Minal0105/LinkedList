609a9aa2b40604ec2746170c8f5073d981c3b1db
minal1593 <minalsalunkhe15@gmail.com>
2023-06-13 17:02:01 +0530

    Added : Code to Create Simple LinkedList

---
+++ src/com/Bridgelabz/LinkedList.java
@@ -0,0 +1,30 @@
+package com.Bridgelabz;
+
+public class LinkedList<T>
+{
+    public Node head;
+    int count = 0;
+    public  void add(T num) {
+        Node newNode = new Node(num);
+        if (head == null) {
+            head = newNode;
+            count++;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        count++;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkedList{" +
+                "head=" + head +
+                ", count=" + count +
+                '}';
+    }
+}
---
+++ src/com/Bridgelabz/Main.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list= new LinkedList();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+
+        System.out.println(list);
+
+    }
+}
